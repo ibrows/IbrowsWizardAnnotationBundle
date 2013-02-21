22317c77fa5744b0d28d3ca1740f0066acdac3e1
Mike Meier <mike.meier@ibrows.ch>
2013-02-21 16:25:28 +0100

    enable twig extension

---
+++ Twig/TwigExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ibrows\Bundle\WizardAnnotationBundle\Twig;
+
+use Ibrows\Bundle\WizardAnnotationBundle\Annotation\AnnotationHandler as Wizard;
+
+class TwigExtension extends \Twig_Extension
+{
+    /**
+     * @var Wizard
+     */
+    protected $wizard;
+
+    /**
+     * @param Wizard $wizard
+     * @return TwigExtension
+     */
+    public function setWizard(Wizard $wizard)
+    {
+        $this->wizard = $wizard;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFunctions(){
+        return array(
+            'getWizard' => new \Twig_Function_Method($this, 'getWizard')
+        );
+    }
+    /**
+     * @return Wizard
+     */
+    public function getWizard()
+    {
+        return $this->wizard;
+    }
+    /**
+     * @return string
+     */
+    public function getName() {
+        return 'cnc_extension';
+    }
+}
